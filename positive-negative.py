cdc320309b48e63ecbe19eef1973c455ae0e72ca
saghan <saghan1@hotmail.com>
2015-09-25 22:38:16 +0530

    Update positive-negative.py

--- positive-negative.py
+++ positive-negative.py
@@ -1,6 +1,8 @@
-# Trying to split punctuation
+
 neg_list=[]
 pos_list=[]
+neg_found=[]
+pos_found=[]
 with open('positive_words.txt','r+') as pos_words_file:
 	
 	for word in pos_words_file:
@@ -11,9 +13,6 @@ with open('negative_words.txt','r+') as neg_words_file:
 	for word in neg_words_file:
 		word=word.lower() 	#convert adjectives to lower case 
 		neg_list.append(word[:len(word)-1])		#remove '\n'	
-#print neg_list
-#print pos_list
-
 positive_count=0
 negative_count=0
 new_word_arr=[]
@@ -29,37 +28,52 @@ with open('input_file.txt','r+') as inputfile:
 			word_len=len(word)
 			for char in word:
 				i+=1
-				if(char=='.'or char==','or char=='!' or char=='?'):
+				if(char=='.'or char==','or char=='!' or char=='?'or char==';' or char==":"):
 					flag=1
-				
 					new_word=word[:i-1]
 					new_word=''.join(new_word)
-				
-				
+					
 					new_word_arr.append(new_word)
 					new_word_arr.append(char)
 					
 				if(i==word_len and flag==0):
 					new_word_arr.append(''.join(word))
 
-		#print "punctuation separation done for a line"
+		# "punctuation separation done for a line"
 		word_arr=new_word_arr
-				
+	print "word_arr=%s" % word_arr			
 for word in word_arr:	
-			if('not' in word_arr[-3]):
-				if(word in pos_list):
-					negative_count+=1
-				if(word in neg_list):
-					positive_count+=1
+			if(word=='not'): 
+				cur_index=word_arr.index(word)
+				print cur_index
+				cur_index+=1
+				while True:
+					if((word_arr[cur_index]  in neg_list) or (word_arr[cur_index]  in pos_list) or (word_arr[cur_index] =='.') or cur_index==word_arr.length+1):
+						break
+					else:
+						cur_index+=1
+						print cur_index
+									
+								
+				if(word_arr[cur_index] in pos_list):
+						neg_found.append('not')
+						neg_found.append(word_arr[cur_index])
+						negative_count+=1
+				if(word_arr[cur_index] in neg_list):
+						pos_found.append('not')
+						pos_found.append(word_arr[cur_index])
+						positive_count+=1
+				del word_arr[word_arr.index('not')]
+				del word_arr[cur_index]
 			
 			else:	
 				if(word in pos_list):
+					pos_found.append(word)
 					positive_count+=1
 				if(word in neg_list):
+					neg_found.append(word)
 					negative_count+=1
-			if(word in pos_list or word in neg_list):
-				adj.append(word)	
-
+			
 if(positive_count>negative_count):
 	print("positive")
 elif(positive_count<negative_count):
@@ -70,4 +84,5 @@ else:
 print "neg count %d" % negative_count
 print "pos count %d" % positive_count
 #print word_arr
-print "adjectives found are %s" %adj
+print "pos adjectives found are %s" %pos_found
+print "neg adjectives found are %s" %neg_found
